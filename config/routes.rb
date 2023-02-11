e1f4b9bec97563d52bda26e4bf61771dde659bc1
Yugie-oh <muthige.l@gmail.com>
2023-02-11 18:50:34 +0200

    added a lot of updates and did DRY

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root "pages#home"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
+
+
+  # When you want to limit some actions, you add only and list the ones you need
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # new and create work hand in hand, new is for the page, create is for the post action
   # edit and update work hand in hand, edit is for the page, update is for the put/patch action
 end
