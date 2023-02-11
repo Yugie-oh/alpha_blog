3a188eb7ae0e679538b85a33e2e0574df79324ba
Yugie-oh <muthige.l@gmail.com>
2023-02-11 12:48:11 +0200

    added the create and edit forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # new and create work hand in hand, new is for the page, create is for the post action
+  # edit and update work hand in hand, edit is for the page, update is for the put/patch action
 end
