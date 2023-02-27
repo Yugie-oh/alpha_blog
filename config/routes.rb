23ab17a8c8d794d2342fb5fa01d8a6d565c2ae08
Yugie-oh <muthige.l@gmail.com>
2023-02-27 09:22:10 +0200

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'about', to: 'pages#about'
   resources :articles
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new] # instead of => post 'users', to: 'users#create'
+  # resources will add CRUD routes
 
   # When you want to limit some actions, you add only and list the ones you need
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
