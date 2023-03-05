e6349bd02f59668aaa8f57ed0a89ea7c74a808cf
Yugie-oh <muthige.l@gmail.com>
2023-03-05 16:15:19 +0200

    moved current_user and logged_in? to the applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # makes the method available in the view
+  helper_method :current_user, :logged_in?
+
+  # Having the current_user method here makes it avalable to all child controllers
+  # Inheritance...
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
