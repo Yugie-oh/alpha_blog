a4c555ad8995c3fdee122a4fed15a5659669b44a
Yugie-oh <muthige.l@gmail.com>
2023-03-05 15:11:23 +0200

    added the login screen and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new] # instead of => post 'users', to: 'users#create'
   # resources will add CRUD routes
 
