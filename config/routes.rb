95a3446a0cc9b3defe2b1cbb9f5daad3ba2fbf71
Yugie-oh <muthige.l@gmail.com>
2023-03-16 09:49:02 +0200

    categories and tests

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # new and create work hand in hand, new is for the page, create is for the post action
   # edit and update work hand in hand, edit is for the page, update is for the put/patch action
+
+  resources :categories, except: [:destroy]
 end
